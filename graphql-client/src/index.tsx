65599802f898830da78b74737d1a6112ea1cd999
MinnieMinwoo <nhs075241@gmail.com>
2023-04-10 17:57:40 +0900

    add graphql client

---
+++ graphql-client/src/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import client from "./routes/client";
+import { ApolloProvider } from "@apollo/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
